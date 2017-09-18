9c6fe4906d71380d3a1e5fb1aa9308d4a64f5c19
Vivek Seth <Vivekseth.m@gmail.com>
2017-09-17 17:19:31 -0700

    Basic webpack-react-babel setup

--- client/index.js
+++ client/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-console.log('hey guys');
+ReactDOM.render(<App />, document.getElementById('root'));
 
